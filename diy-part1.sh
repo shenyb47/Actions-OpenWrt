fd169b0a33789b76e6040aca08496f58d3b10e09
liuyishan2019@gmail.com <52615712+yishan2019@users.noreply.github.com>
2021-07-07 11:51:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-sed -i 's#$(Device/tplink-4mlzma)#$(Device/tplink-16mlzma)#g' target/linux/ar71xx/image/tiny-tp-link.mk
-sed -i 's#$(Device/tplink-8mlzma)#$(Device/tplink-16mlzma)#g' target/linux/ar71xx/image/tiny-tp-link.mk
-sudo sed -i 's/# CONFIG_ATH79_MACH_TL_WR841N_V8 is not set/CONFIG_ATH79_MACH_TL_WR841N_V8=y/g' target/linux/ar71xx/config-4.14
-sudo sed -i '588a \ \ DEVICE_PACKAGES := kmod-usb-core kmod-usb2' target/linux/ar71xx/image/tiny-tp-link.mk
-sudo sed -i '222a \ \t ath79_register_usb();' target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr841n-v8.c
-sudo sed -i 's#AR934X_ETH_CFG_SW_PHY_SWAP#AR934X_ETH_CFG_SW_ONLY_MODE#g' target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr841n-v8.c
+
