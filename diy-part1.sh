cd706c6beaa6d85e721ab891295bf478bd67f232
liuyishan2019@gmail.com <52615712+yishan2019@users.noreply.github.com>
2021-10-21 16:25:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,13 +14,13 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo >> feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-echo "src-git smartdns https://github.com/pymumu/smartdns.git" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
+# echo "src-git smartdns https://github.com/pymumu/smartdns.git" >> feeds.conf.default
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
 # echo "src-git clone https://github.com/tuanqing/install-program package/install-program" >> feeds.conf.default
 echo "src-git AdGuardHome https://github.com/AdguardTeam/AdGuardHome" >> feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
-sed -i 's#$(Device/tplink-4mlzma)#$(Device/tplink-16mlzma)#g' target/linux/ath79/image/tiny-tp-link.mk
-sed -i 's#$(Device/tplink-8mlzma)#$(Device/tplink-16mlzma)#g' target/linux/ath79/image/tiny-tp-link.mk
+# sed -i 's#$(Device/tplink-4mlzma)#$(Device/tplink-16mlzma)#g' target/linux/ath79/image/tiny-tp-link.mk
+# sed -i 's#$(Device/tplink-8mlzma)#$(Device/tplink-16mlzma)#g' target/linux/ath79/image/tiny-tp-link.mk
 # sed -i '6a \ \ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_SW_ONLY_MODE);' target/linux/ath79/dts/ar9341_tplink_tl-wr841-v8.dts （语法错）
 
