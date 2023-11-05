407c5549bfa2217e505ae91e8d04f89ead0a5cd9
liuyishan2019@gmail.com <52615712+yishan2019@users.noreply.github.com>
2023-11-05 23:06:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,8 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # echo >> feeds.conf.default echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
